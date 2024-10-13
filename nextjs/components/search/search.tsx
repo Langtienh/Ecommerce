268c1f6314e720799aae7bd15ef27ad0ded7ab81
langtienh <tien.lt225933@sis.hust.edu.vn>
2024-10-14 00:19:18 +0700

    fix search params

--- nextjs/components/search/search.tsx
+++ nextjs/components/search/search.tsx
@@ -1,37 +1,43 @@
-"use client";
+'use client'
 
-import { cn } from "@/lib/utils";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useDebouncedCallback } from "use-debounce";
-import { Input } from "../ui/input";
+import { cn } from '@/lib/utils'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
+import { useDebouncedCallback } from 'use-debounce'
+import { Input } from '../ui/input'
 
 interface SearchInputProps {
-  className?: string;
-  placeholder?: string;
+  className?: string
+  placeholder?: string
 }
-export default function SearchInput({
-  className,
-  placeholder,
-}: SearchInputProps) {
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
-  const { replace } = useRouter();
+function Search({ className, placeholder }: SearchInputProps) {
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+  const { replace } = useRouter()
   const handleSearch = useDebouncedCallback((term: string) => {
-    const params = new URLSearchParams(searchParams);
-    params.set("page", "1");
+    const params = new URLSearchParams(searchParams)
+    params.set('page', '1')
     if (term) {
-      params.set("search", term);
+      params.set('search', term)
     } else {
-      params.delete("search");
+      params.delete('search')
     }
-    replace(`${pathname}?${params.toString()}`);
-  }, 500);
+    replace(`${pathname}?${params.toString()}`)
+  }, 500)
   return (
     <Input
       onChange={(e) => handleSearch(e.target.value)}
-      defaultValue={searchParams.get("search")?.toString()}
+      defaultValue={searchParams.get('search')?.toString()}
       placeholder={placeholder}
-      className={cn("", className)}
+      className={cn('', className)}
     />
-  );
+  )
+}
+
+export default function SearchInput(props: SearchInputProps) {
+  return (
+    <Suspense fallback={<Input placeholder={props.placeholder} className={cn('', props.className)} />}>
+      <Search {...props} />
+    </Suspense>
+  )
 }
